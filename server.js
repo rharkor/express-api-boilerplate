6d9606c3458c6b8266b8cf75a2c46df480a8dfa8
Louis Huort <louis.huort@etudiant.univ-lr.fr>
2022-05-18 22:59:22 +0200

    update: Change the host

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const app = require("./index");
 const port = process.env.PORT;
-const host = process.env.HOST;
 const errorMiddleware = require("./middlewares/error.middleware");
 
 console.log(`Node environment: ${process.env.NODE_ENV}`);
-app.listen(port, host, () => {
-    console.log(`Example app listening at port http://${host}:${port}`);
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
 });
 
 // Error Handler Middleware
